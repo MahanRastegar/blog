ab8131fec599347fd50761d5202714abbb370b20
Mahan Rastegar <the.mahan.rastegar@gmail.com>
2026-03-28 00:01:23 +0000

    astro.config.mjs: changed url

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://mahanrastegar.github.io/blog/",
-	base: "/blog/",
+	site: "https://mahanrastegar.github.io",
+	base: "/blog",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
